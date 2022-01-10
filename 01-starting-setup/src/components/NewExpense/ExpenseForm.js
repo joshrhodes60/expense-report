d30267f9cde093942fb68c0af1ba1749c80ec97b
“josh” <joshrhodes60@icloud.com>
2022-01-10 13:59:42 -0600

    Expense project complete

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-     const [enteredTitle, setEnteredTitle]= useState('');
-      const [enteredAmount, setEnteredAmount]=useState('');
-      const [enteredDate, setEnteredDate]=useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
   // const [userInput, setUserInput] = useState({
   //   enteredTitle: "",
   //   enteredAmount: "",
@@ -29,7 +29,6 @@ const ExpenseForm = (props) => {
     //   ...userInput,
     //   enteredAmount: event.target.value,
     // });
-    
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
@@ -44,25 +43,25 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
-      date: new Date(enteredDate)
+      amount: +enteredAmount,
+      date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
-    <form onSubmit={ submitHandler}>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input 
-          type="text"
-          value={enteredTitle} 
-          onChange={titleChangeHandler} 
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
           />
         </div>
       </div>
@@ -91,6 +90,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
